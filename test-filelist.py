4182fec3eaff7219fd94a1f2ae4aba8c7d7308b6
nico82d4 <nico421091@gmail.com>
2020-02-24 15:18:07 +0900

    引数のパス存在確認と、細かな修正など。

--- test-filelist.py
+++ test-filelist.py
@@ -5,7 +5,7 @@ import datetime
 import re
 
 
-
+# 引数のパスからハッシュ値を生成して、先頭8文字だけを返す関数。
 def create_hash(path):
     digestFirst = ''
 
@@ -32,35 +32,36 @@ def create_hash(path):
 
 
 
-def show_recursive(path):
-    for po in path.iterdir():
+# リネーム用の関数
+def file_rename(path):
+    for tmpfile in path.iterdir():
 
         # ディレクトリの場合、関数を再帰的に呼び出す。
-        if po.is_dir():
-            show_recursive(po)
+        if tmpfile.is_dir():
+            file_rename(tmpfile)
 
         # ファイルの場合
-        elif po.is_file():
+        elif tmpfile.is_file():
             # ファイル名の取得
-            name_old = po.name
+            name_old = tmpfile.name
 
             # 拡張子の取得
-            suffix = po.suffix
+            suffix = tmpfile.suffix
 
             # 処理を行う拡張子を正規表現でチェック
             # （今回はマジックナンバーではなく、拡張子で。）
-            result = re.match(r'\.(?:py|txt|md)', suffix)
+            result = re.match(r'\.(?:py|txt|md)$', suffix)
 
             # 対象の拡張子である場合
             if result is not None:
 
                 # 最終更新日付の取得と表示形式の変更
-                ts1 = po.stat().st_mtime
+                ts1 = tmpfile.stat().st_mtime
                 ts2 = datetime.datetime.fromtimestamp(ts1)
                 timestamp = ts2.strftime('%Y-%m-%d-%H%M%S')
 
                 # ファイルのハッシュ値を取得
-                digest = create_hash(po)
+                digest = create_hash(tmpfile)
 
                 # 新しいファイル名の生成
                 # （joinメソッドの区切りなしで生成。）
@@ -71,16 +72,22 @@ def show_recursive(path):
 
 
 
-
 # コマンドライン引数が1つだけの場合は、カレントパスを対象とする。
 if len(sys.argv) == 1:
     path = pathlib.Path('.')
-    show_recursive(path)
+    file_rename(path)
 
 # コマンドライン引数が2つの場合は、引数のパスを対象とする。
 elif len(sys.argv) == 2:
     path = pathlib.Path(sys.argv[1])
-    show_recursive(path)
+
+    # 引数に指定されたパスが存在する場合は、処理を続行。
+    if path.exists():
+        file_rename(path)
+
+    # 引数に指定されたパスが存在しない場合、メッセージを出力する。
+    else:
+        print('パスが存在しません。', sys.argv[1])
 
 # コマンドライン引数が1と2以外の場合は、メッセージだけ出力する。
 else:
